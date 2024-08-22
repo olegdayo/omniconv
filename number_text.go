be64d32d5ad71e440f41bed707a75e778b647440
olegsama <ol-sidorenkov@mail.ru>
2024-08-22 22:42:38 +0300

    must

--- number_text.go
+++ number_text.go
@@ -16,6 +16,8 @@ func FloatToStringConverter[T Float](from T) (to string) {
 }
 
 // StringToIntConverter parses int from string
+//
+// On error returns default value (0)
 func StringToIntConverter[T Int](from string) (to T) {
 	n, err := strconv.Atoi(from)
 	if err != nil {
@@ -24,12 +26,37 @@ func StringToIntConverter[T Int](from string) (to T) {
 	return T(n)
 }
 
+// MustStringToIntConverter parses int from string
+//
+// On error panics
+func MustStringToIntConverter[T Int](from string) (to T) {
+	n, err := strconv.Atoi(from)
+	if err != nil {
+		panic(err)
+	}
+	return T(n)
+}
+
 // StringToIntConverter parses float from string
+//
+// On error returns default value (0)
 func StringToFloatConverter[T Float](from string) (to T) {
 	const bitSize = 64
 	n, err := strconv.ParseFloat(from, bitSize)
 	if err != nil {
 		return 0
+	}
+	return T(n)
+}
+
+// MustStringToFloatConverter parses float from string
+//
+// On error panics
+func MustStringToFloatConverter[T Float](from string) (to T) {
+	const bitSize = 64
+	n, err := strconv.ParseFloat(from, bitSize)
+	if err != nil {
+		panic(err)
 	}
 	return T(n)
 }
