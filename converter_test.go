c413a8f4f276c4c75f7c6917e1f70f7fd83a5681
olegsama <ol-sidorenkov@mail.ru>
2024-09-25 19:23:17 +0300

    more tests

--- converter_test.go
+++ converter_test.go
@@ -6,24 +6,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTextConverterStringToBytes(t *testing.T) {
-	type inputType = string
-	type outputType = []byte
+func TestConvertSlice(t *testing.T) {
+	type inputType = int
+	type outputType = string
 
 	testCases := []struct {
 		name   string
-		input  inputType
-		output outputType
+		input  []inputType
+		output []outputType
 	}{
 		{
 			name:   "empty",
-			input:  "",
-			output: []byte{},
+			input:  []inputType{},
+			output: []outputType{},
 		},
 		{
 			name:   "non-empty",
-			input:  "hello",
-			output: []byte{'h', 'e', 'l', 'l', 'o'},
+			input:  []inputType{1, 2, 3},
+			output: []outputType{"1", "2", "3"},
 		},
 	}
 
@@ -36,7 +36,45 @@ func TestTextConverterStringToBytes(t *testing.T) {
 				assert.Equal(
 					t,
 					testCase.output,
-					TextConverter[inputType, outputType](testCase.input),
+					ConvertSlice(testCase.input, IntToStringConverter[inputType]),
+				)
+			},
+		)
+	}
+}
+
+func TestConvertMap(t *testing.T) {
+	type key = byte
+	type inputType = int
+	type outputType = string
+
+	testCases := []struct {
+		name   string
+		input  map[key]inputType
+		output map[key]outputType
+	}{
+		{
+			name:   "empty",
+			input:  map[key]inputType{},
+			output: map[key]outputType{},
+		},
+		{
+			name:   "non-empty",
+			input:  map[key]inputType{16: 1, 32: 2, 64: 3},
+			output: map[key]outputType{16: "1", 32: "2", 64: "3"},
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+		t.Run(
+			testCase.name,
+			func(t *testing.T) {
+				t.Parallel()
+				assert.Equal(
+					t,
+					testCase.output,
+					ConvertMap(testCase.input, IntToStringConverter[inputType]),
 				)
 			},
 		)
